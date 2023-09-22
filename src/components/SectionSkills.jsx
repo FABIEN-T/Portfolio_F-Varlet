6f970fc025ae413ebb7f20136ff9a9714e40c4ad
fabT <fabien.varlet@live.fr>
2023-09-22 15:26:51 +0200

    dataProjects.js

--- src/components/SectionSkills.jsx
+++ src/components/SectionSkills.jsx
@@ -5,7 +5,7 @@ export default function SectionSkills() {
   return (
     <section
       id="competences"
-      className="flex flex-col bg-myBlue mb-12 px-4 pt-14 pb-20 sm:mb-14 md:pt-20"
+      className="flex flex-col bg-gradient-to-br from-rose-600 to-myBlue mb-12 px-4 pt-14 pb-20 sm:mb-14 md:pt-20"
     >
       <h2 className="text-white text-2xl mx-auto mb-7 sm:text-4xl sm:mb-10 md:text-5xl md:mb-12">
         Mes Compétences
@@ -13,7 +13,7 @@ export default function SectionSkills() {
 
       <ul className="grid mx-auto grid-cols-3 gap-3 sm:grid-cols-4 sm:gap-4 lg:grid-cols-6 lg:max-w-3xl">
         {datasSkills.map(({ id, skill, logoSvg }) => (
-          <SkillCard key={id}skill={skill} logoSvg={logoSvg} />
+          <SkillCard key={id} skill={skill} logoSvg={logoSvg} />
         ))}
       </ul>
     </section>
